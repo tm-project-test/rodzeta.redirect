1085368ff662f338d6b82336b7aef4f0662e1f79
Rick Deckard <xxxblasteroid@gmail.com>
2016-06-26 21:30:39 +0300

    fix

---
+++ .last_version/lang/ru/options.php
@@ -0,0 +1,7 @@
+<?php
+defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+
+$MESS["RODZETA_REDIRECT_MAIN_TAB_SET"] = "Настройки";
+$MESS["RODZETA_REDIRECT_MAIN_TAB_TITLE_SET"] = "Настройка параметров модуля";
+$MESS["RODZETA_REDIRECT_OPTIONS_SAVED"] = "Настройки сохранены";
+$MESS["RODZETA_REDIRECT_OPTIONS_RESETED"] = "Кеш редиректов очищен";
