c02c1e9e30a1190a1872dfac389d1638032e3e2d
Rick Deckard <xxxblasteroid@gmail.com>
2016-07-15 08:13:11 +0300

    rename

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,5 +1,5 @@
 <?php
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
 
-$MESS["RODZETA_REDIRECT_MODULE_NAME"] = "SEO редиректы";
+$MESS["RODZETA_REDIRECT_MODULE_NAME"] = "Редиректы из csv-файла для SEO";
 $MESS["RODZETA_REDIRECT_MODULE_DESCRIPTION"] = "Редиректы со старых урл на новые";
