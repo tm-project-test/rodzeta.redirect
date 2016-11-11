e3d42f14abaf53c6dfcba4ad5cd00dee81d000f8
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-11 16:27:41 +0300

    fix

--- include.php
+++ include.php
@@ -17,7 +17,7 @@ EventManager::getInstance()->addEventHandler("main", "OnBeforeProlog", function 
 		return;
 	}
 
-	$protocol = !empty($_SERVER["HTTPS"])? "https" : "http";
+	$protocol = !empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] != "off" ? "https" : "http";
 	$host = $_SERVER["SERVER_NAME"];
 	$port = !empty($_SERVER["SERVER_PORT"]) && $_SERVER["SERVER_PORT"] != "80"? (":" . $_SERVER["SERVER_PORT"]) : "";
 	$url = null;
