aaf22fd2ee5ceef137699df653f61acf9bb98e73
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-25 13:16:27 +0400

    fix

--- install/admin/rodzeta.redirect.php
+++ install/admin/rodzeta.redirect.php
@@ -1,3 +1,3 @@
 <?php
 
-require $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/rodzeta.redirect/admin/rodzeta.redirect/index.php";
+require $_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/rodzeta.redirect/admin/rodzeta.redirect.php";
