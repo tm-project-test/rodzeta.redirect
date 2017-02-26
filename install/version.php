3179800af7dcd3c94a2734c738b4aedf1129ae30
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2017-02-26 15:52:49 +0300

    fix

--- install/version.php
+++ install/version.php
@@ -3,6 +3,6 @@
 // NOTE this file must compatible with php 5.3
 
 $arModuleVersion = array(
-  'VERSION' 		=> '2017.02.25',
-  'VERSION_DATE' 	=> '2017-02-25'
+  'VERSION' 		=> '2017.02.26',
+  'VERSION_DATE' 	=> '2017-02-26'
 );
